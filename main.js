453084061fe9f0d52947ce1f7d85ead43b8dc85b
scrof90 <asknyshev@gmail.com>
2021-09-15 01:53:34 +0300

    add basic styling and basic functions

---
+++ main.js
@@ -0,0 +1,20 @@
+function operate(operator, a, b) {
+  return operator(a, b);
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) return 'ERROR';
+  return a % b;
+}
