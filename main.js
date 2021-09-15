24c35a05d23cfe9acd8d468a65a46947b77711ee
scrof90 <asknyshev@gmail.com>
2021-09-15 05:10:12 +0300

    implement all right-hand operators

--- main.js
+++ main.js
@@ -1,5 +1,91 @@
-function operate(operator, a, b) {
-  return operator(a, b);
+let displayValue = '0';
+let valueA = '';
+let valueB = '';
+let operator = null;
+let waitingForA = true;
+
+const numBtns = Array.from(document.querySelectorAll('.num'));
+numBtns.forEach((btn) => (btn.onclick = evalNum));
+
+const opBtns = Array.from(document.querySelectorAll('.op'));
+opBtns.forEach((btn) => (btn.onclick = evalOperator));
+
+const equalsBtn = document.getElementById('equals');
+equalsBtn.onclick = operate;
+
+const clearBtn = document.getElementById('clear');
+clearBtn.onclick = clear;
+
+// TODO: refactor the whole thing to make an array of numbers and operators
+
+function evalNum(e) {
+  const num = e.target.textContent;
+  if (displayValue === '0' && num === '0') {
+    displayValue = '0';
+  } else if (displayValue === '0' && num !== '0') {
+    displayValue = num;
+  } else {
+    displayValue += num;
+  }
+  if (waitingForA) {
+    valueA = displayValue;
+  } else {
+    valueB = displayValue;
+  }
+  updateScreen();
+}
+
+function evalOperator(e) {
+  const op = e.target.textContent;
+  switch (op) {
+    case '/':
+      operator = divide;
+      break;
+    case '*':
+      operator = multiply;
+      break;
+    case '-':
+      operator = subtract;
+      break;
+    case '+':
+      operator = add;
+      break;
+    case 'x<sup>y</sup>':
+      operator = exponent;
+      break;
+  }
+  waitingForA = false;
+  displayValue = '';
+}
+
+function clear(e) {
+  displayValue = '0';
+  valueA = '0';
+  valueB = '0';
+  operator = null;
+  waitingForA = true;
+  updateScreen();
+}
+
+function updateScreen() {
+  const screen = document.getElementById('screen');
+  screen.textContent = displayValue;
+}
+
+function operate(e) {
+  if (!operator) return;
+  const a = +valueA;
+  const b = +valueB;
+  if (operator === divide && b === 0) {
+    displayValue = 'NO';
+    valueA = '';
+  } else {
+    const result = operator(a, b);
+    displayValue = result;
+    valueA = result;
+  }
+  updateScreen();
+  waitingForA = true;
 }
 
 function add(a, b) {
@@ -15,6 +101,9 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  if (b === 0) return 'ERROR';
-  return a % b;
+  return a / b;
+}
+
+function exponent(a, b) {
+  return a ** b;
 }
